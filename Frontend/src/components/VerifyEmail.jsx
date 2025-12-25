b0d868b4d681838e737d0c613026a81734c0bbd0
Rushikesh Gude <rushirdj23@gmail.com>
2025-12-26 02:36:14 +0530

    updated verifyEmail

--- Frontend/src/components/VerifyEmail.jsx
+++ Frontend/src/components/VerifyEmail.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { motion } from "framer-motion";
 const API_BASE = import.meta.env.VITE_API_URL;
 
 
