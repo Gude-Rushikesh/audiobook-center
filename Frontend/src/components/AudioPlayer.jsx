9822b18c811e20f19bd415d44af8e4b605e0b5c5
Rushikesh Gude <rushirdj23@gmail.com>
2026-01-05 20:28:32 +0530

    button adding next and previous

--- Frontend/src/components/AudioPlayer.jsx
+++ Frontend/src/components/AudioPlayer.jsx
@@ -1064,7 +1064,7 @@ export default function AudioPlayer({
   return (
 
     <div className="fixed bottom-0 left-0 right-0 z-50"
-      onClick={() => {if (window.innerWidth < 768 && !IsExpanded) {setIsExpanded(true);}}}>
+      onClick={() => {if (window.innerWidth < 768 && !isExpanded) {setIsExpanded(true);}}}>
       <div className="backdrop-blur-xl bg-black/90 border-t border-white/10">
         <div className="max-w-6xl mx-auto px-6 py-4 space-y-3 text-white">
 
@@ -1126,7 +1126,7 @@ export default function AudioPlayer({
 
 
             {/* ELAPSED */}
-            <span className="text-xs text-white/70 w-14 tabular-nums text-right">
+            <span className="text-lg text-white/70 w-14 tabular-nums text-right">
               {formatTime(currentTime)}
             </span>
 
@@ -1146,7 +1146,7 @@ export default function AudioPlayer({
 
 
             {/* TOTAL */}
-            <span className="text-xl text-white/70 w-14 tabular-nums">
+            <span className="text-lg text-white/70 w-14 tabular-nums">
               {formatTime(totalDuration)}
             </span>
 
@@ -1159,7 +1159,7 @@ export default function AudioPlayer({
                 e.stopPropagation();
                 jumpBackward();
               }}
-              className="text-xl px-2 py-1 rounded-md
+              className="text-lg px-3 py-2 rounded-md
                         bg-white/10 hover:bg-white text-black transition"
               >
                 ↺30s
@@ -1170,7 +1170,7 @@ export default function AudioPlayer({
                   e.stopPropagation();
                   jumpForward();
                 }}
-                className="text-xl px-2 py-1 rounded-md
+                className="text-lg px-3 py-2 rounded-md
                           bg-white/10 hover:bg-white text-black transition"
               >
                 30s↻
@@ -1181,7 +1181,7 @@ export default function AudioPlayer({
                 e.stopPropagation();
                 changeSpeed();
               }}
-              className="text-xl px-3 py-1 rounded-md
+              className="text-lg px-3 py-2 rounded-md
                          bg-white/10 hover:bg-white/20 transition"
               >
                 {SPEEDS[speedIndex]}x
@@ -1190,7 +1190,7 @@ export default function AudioPlayer({
           </div>
 
           {isBuffering && (
-            <p className="text-xs text-white/50">
+            <p className="text-xl text-white/50">
               Buffering audio…
             </p>
           )}
