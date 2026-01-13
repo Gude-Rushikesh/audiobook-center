71eb440b573c9b8590debff73e2a4c8da5e3f312
Rushikesh Gude <rushirdj23@gmail.com>
2026-01-14 00:38:58 +0530

    redesigning the collection page

--- Frontend/src/components/BookToAudio.jsx
+++ Frontend/src/components/BookToAudio.jsx
@@ -360,7 +360,7 @@ export default function BookToAudio() {
       <section className="flex flex-col items-center justify-center
                           text-center px-6 sm:px-6 pt-16 sm:pt-18 pb-20 sm:pb-28">
 
-        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-display font-semibold text-gray-900 leading-tight">
+        <h2 className="text-3xl sm:text-4xl md:text-6xl lg:text-6xl font-display font-semibold text-white leading-tight">
           Welcome to Audiobook Center
         </h2>
 
