6f3225e33c512ff1031b8bf2c143bccc3e3ff51b
Rushikesh Gude <rushirdj23@gmail.com>
2026-02-01 11:30:16 +0530

    colour design

--- Frontend/src/components/BookToAudio.jsx
+++ Frontend/src/components/BookToAudio.jsx
@@ -153,10 +153,10 @@ const CATEGORY_MAP = [
     ];
 
     const CATEGORY_STYLES = {
-        "Fantasy & Epics": "bg-indigo-400 text-black font-semibold",
-        "Romance & Emotion": "bg-rose-400 text-black font-semibold",
-        "Philosophy & Life": "bg-amber-300 text-black font-semibold",
-        "Classics": "bg-emerald-400 text-black font-semibold",
+        "Fantasy & Epics": "bg-indigo-500 text-black font-semibold",
+        "Romance & Emotion": "bg-rose-700 text-black font-semibold",
+        "Philosophy & Life": "bg-amber-400 text-black font-semibold",
+        "Classics": "bg-emerald-500 text-black font-semibold",
         "Science Fiction": "bg-sky-400 text-black font-semibold",
         };
 
